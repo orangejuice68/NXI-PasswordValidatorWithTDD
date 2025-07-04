7fd372f134762b84b69268f81054dd5f9f921fc9
orangejuice68 <srithep.j@ku.th>
2025-07-04 18:37:23 +0700

    Add files via upload

--- App.java
+++ App.java
@@ -1,7 +1,5 @@
 import lib.*;
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!!");
-        //PasswordValidatorTest.run();
+public class App { public static void main(String[] args) {
+        PasswordValidatorTest.run();
     }
 }
